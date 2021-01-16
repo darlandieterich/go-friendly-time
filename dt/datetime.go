540f39172278940cabb66a07896a1f4644e63bdd
darlandieterich <darlandieterich@gmail.com>
2021-01-16 15:46:07 -0300

    Added pkg to datetime

---
+++ dt/datetime.go
@@ -0,0 +1,12 @@
+package dt
+
+const (
+	Hour         string = "15"
+	Minute       string = "04"
+	Second       string = "05"
+	Day          string = "02"
+	Month        string = "01"
+	Year         string = "2006"
+	FullDate     string = "2006-01-02"
+	FullDateTime string = "2006-01-02 15:04:05"
+)

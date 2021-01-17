2bf92117a9135349dd1fc8c10ea4c54879e77c20
darlandieterich <darlandieterich@gmail.com>
2021-01-17 09:42:08 -0300

    Running example

--- example/main.go
+++ example/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"log"
+	"time"
+
 	ft "github.com/darlandieterich/go-friendly-time"
 )
 
 func main() {
-	/log.Println("oi")
+	log.Println("oi")
 	log.Println(time.Now().Format(ft.Hour))
 }

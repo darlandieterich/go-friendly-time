0a1d293370619e30d2ee0bb3c0ba06970ae50ddf
darlandieterich <darlandieterich@gmail.com>
2021-01-19 22:57:37 -0300

    Added time now string and struct format

--- ft.go
+++ ft.go
@@ -2,13 +2,23 @@ package ft
 
 import "time"
 
+// TypeFT struc of time
 type TypeFT struct {
 	Time   time.Time
 	Locale *time.Location
 }
 
+// CalcDate calculate the date sum or subtract dates
 func (t TypeFT) CalcDate(dateEnd time.Time) time.Time {
 	return t.Time.AddDate(dateEnd.Year(), int(dateEnd.Month()), dateEnd.Day())
 }
 
-//get time now
+// GetTimeNowString return time now in string format
+func (t TypeFT) GetTimeNowString(format string) string {
+	return time.Now().In(t.Locale).Format(format)
+}
+
+// GetTimeNowStruct return time now in struct format
+func (t TypeFT) GetTimeNowStruct(format string) time.Time {
+	return time.Now().In(t.Locale)
+}
